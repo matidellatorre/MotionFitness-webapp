a4d28b27c67115554af11d9a03aeb2f959d324f9
Matías Della Torre <matidellatorre@gmail.com>
2022-09-28 18:17:38 -0300

    Spacers added to the app-bar

--- src/components/CustomHeader.vue
+++ src/components/CustomHeader.vue
@@ -1,17 +1,21 @@
 <template>
-  <v-app-bar app color="primary" dark>
-    <div class="d-flex justify-space-between">
-      <TopLeftlogo />
-      <v-toolbar-items class="hidden-sm-and-down">
+  <nav>
+    <v-toolbar flat app dark color="primary">
+      <v-toolbar-title>
+        <TopLeftlogo />
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
         <v-btn text> Routines </v-btn>
         <v-divider vertical></v-divider>
         <v-btn text> Explore </v-btn>
         <v-divider vertical></v-divider>
         <v-btn text> FAQ </v-btn>
       </v-toolbar-items>
+      <v-spacer></v-spacer>
       <v-btn color="secondary" elevation="10" rounded>Log in</v-btn>
-    </div>
-  </v-app-bar>
+    </v-toolbar>
+  </nav>
 </template>
 
 <script>
