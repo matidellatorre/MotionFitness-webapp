3847a297797aa0cdcad8efc1021563c98c8c1c96
gastialasia <galasia@itba.edu.ar>
2022-09-30 10:31:40 -0300

    Habemus Router

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,13 @@
 <template>
-  <v-app>
+  <v-app id="app">
     <CustomHeader />
-    <v-main id="main-content">
-      <WelcomeCard />
-    </v-main>
+    <router-view />
   </v-app>
 </template>
-
 <script>
 import CustomHeader from "@/components/CustomHeader";
-import WelcomeCard from "@/components/WelcomeCard";
 export default {
-  name: "App",
-
-  components: {
-    CustomHeader,
-    WelcomeCard,
-  },
-
-  data: () => ({
-    //
-  }),
+  components: { CustomHeader },
 };
 </script>
-
-<style>
-#main-content {
-  background: url("../src/assets/bg.png");
-  background-size: cover;
-}
-#sheet-container {
-  margin-top: 5%;
-  margin-left: 5%;
-}
-</style>
+<style></style>
