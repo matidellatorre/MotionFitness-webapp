4c4e379adc4d06c213e5764e7e9186ba4ce060fb
gastialasia <galasia@itba.edu.ar>
2022-10-05 15:41:11 -0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -31,7 +31,7 @@ const routes = [
     component: RoutinesView,
   },
   {
-    path: "/routines/details/:id",
+    path: "/routines/:id",
     name: "details",
     component: RoutineDetails,
   },
