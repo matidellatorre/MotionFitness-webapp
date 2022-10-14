d2687bd635d147871e70a68f5b087228e782f218
gastialasia <galasia@itba.edu.ar>
2022-10-14 14:20:28 -0300

    Unfinished add exercise

--- src/components/CreateExercisePopUp.vue
+++ src/components/CreateExercisePopUp.vue
@@ -11,21 +11,24 @@
           <v-btn width="100px" v-bind:color="isRest==false ? 'secondary' : 'white'" @click="isRest=false">Exercise</v-btn>
           <v-btn width="100px" v-bind:color="isRest==true ? 'secondary' : 'white'" @click="isRest=true">Rest</v-btn>
         </div>
-        <v-text-field type="text" label="Name" v-model="exercise.name"/>
-        <v-text-field v-if="isRest==false" type="number" label="Reps" v-model.number="cycleExercise.repetitions" />
-        <v-text-field type="number" label="Duration" v-model.number="cycleExercise.duration" />
-        <div>
-          <v-combobox
-            v-model="selectedExercise"
-            :items="this.$exercises"
-            item-text="name"
-            item-value="id"
-            dense
-            hide-no-data
-            filled
-            label="Filled"
-          ></v-combobox>
-        </div>
+        <v-form @submit.prevent="handleSubmit">
+          <v-text-field type="text" label="Name" v-model="exercise.name"/>
+          <v-text-field v-if="isRest==false" type="number" label="Reps" v-model.number="cycleExercise.repetitions" />
+          <v-text-field type="number" label="Duration" v-model.number="cycleExercise.duration" />
+          <div>
+            <v-combobox
+              v-model="selectedExercise"
+              :items="this.$exercises"
+              item-text="name"
+              item-value="id"
+              @input="handleInput"
+              dense
+              hide-no-data
+              filled
+              label="Filled"
+            ></v-combobox>
+          </div>
+        </v-form>
         <div class="d-flex justify-center my-3">
           <v-btn @click="$emit('popUpClosed'); create()" class="mr-5 rounded-xl" color="secondary">Create Cycle</v-btn>
           <v-btn outlined dark color="red" @click="$emit('popUpClosed')" class="ml-5 rounded-xl">Cancel</v-btn>
@@ -36,7 +39,7 @@
 </template>
 
 <script>
-import {Exercise, ExerciseApi} from "@/api/exercise";
+import {Exercise} from "@/api/exercise";
 import {CycleExercise} from "@/api/cycleExercise";
 import { mapActions, mapState } from "pinia";
 import { useExerciseStore } from "@/store/ExerciseStore";
@@ -57,15 +60,26 @@ export default {
     show: Boolean,
   },
   methods: {
+    handleInput () {
+      this.$nextTick(() => {
+        console.log(this.selectedExercise)
+      })
+    },
+    handleSubmit () {
+      this.selectedExercise = this.value
+    },
     create() {
-      console.log(this.selectedExercise)
-      const res = ExerciseApi.add(this.exercise)
-      console.log(res)
-      // Se crea bien el ejercicio, falta importar y usar el store de cycleExercises
-      // Creo que np hace falta tener un store para Exercise, solo para CycleExercise
-      // if (res.id) {
-      //   CycleExerciseApi.add(3,res.id,this.cycleExercise)
-      // }
+      if(this.selectedExercise.id){
+        console.log("el ejercicio esta creado");
+        //Llamo a la funcion de createCycle
+      } else {
+
+        console.log("creemos uno nuevo");
+        // const res = ExerciseApi.add(this.exercise);
+        // console.log(res);
+      }
+
+
     },
     ...mapActions(useExerciseStore, {
       $getAllExercises: 'getAll',
