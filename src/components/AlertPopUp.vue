928efb015074f32bfa59f4dc43e4d147837c247b
Matías Della Torre <matidellatorre@gmail.com>
2022-10-16 12:18:07 -0300

    Create routine validation

--- src/components/AlertPopUp.vue
+++ src/components/AlertPopUp.vue
@@ -7,7 +7,9 @@
       <v-card class="pa-10 rounded-xl text-center">
         <h1 class="text-center secondary--text mb-5">Error</h1>
         <h4 class="text-center mlt-4 mb-8">{{this.message?this.message:'Sorry, we have an internal error. Please try again later'}}</h4>
+        <div class="d-flex text-center align-center justify-center">
         <v-btn @click="$emit('popUpClosed');" class="mr-5 rounded-xl flex-fill" color="secondary">OK</v-btn>
+        </div>
       </v-card>
     </v-dialog>
   </div>
